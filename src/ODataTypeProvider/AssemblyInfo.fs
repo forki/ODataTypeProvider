47e4237644c5bfbf8a4b62bce4961372400f3991
Christopher Atkins <christopher316@gmail.com>
2016-04-06 13:00:02 -0400

    initialize from project scaffold

---
+++ src/ODataTypeProvider/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("ODataTypeProvider")>]
+[<assembly: AssemblyProductAttribute("ODataTypeProvider")>]
+[<assembly: AssemblyDescriptionAttribute("an F# type provider for OData resources")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
+    let [<Literal>] InformationalVersion = "1.0"
